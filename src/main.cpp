492ffa6ef28938e5458915c8d3b6cc079ff0186f
“Mmeche” <mattherb@live.fr>
2023-11-23 00:18:54 +0100

    Oublie de push le main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 
-//#include <Eigen/Dense>
-#include "C:/Users/fox20/Downloads/eigen-3.4.0/eigen-3.4.0/Eigen/Dense"
+#include <Eigen/Dense>
+//#include "C:/Users/fox20/Downloads/eigen-3.4.0/eigen-3.4.0/Eigen/Dense"
 
 #include "Geogebra_conics.hpp"
 #include "class.hpp"
@@ -22,112 +22,99 @@ int main()
   viewer.show_value(false);
   viewer.show_label(true);
 
-  /*// draw points anciennement le template du prof
-  //conique ellipse
-  Eigen::VectorXd pt1(3), pt2(3), pt3(3), pt4(3), pt5(3);
-  pt1 <<  1.5,  2.0, 1.0;
-  pt2 <<  3.0,  1.0, 1.0;
-  pt3 << -2.0, -1.0, 1.0;
-  pt4 <<  0.5, -2.0, 1.0;
-  pt5 << -1.0,  3.0, 1.0;
-
-  viewer.push_point(pt1, "p1", 200,0,0);
-  viewer.push_point(pt2, "p2", 200,0,0);
-  viewer.push_point(pt3, "p3", 200,0,0);
-  viewer.push_point(pt4, "p4", 200,0,0);
-  viewer.push_point(pt5, "p5", 200,0,0);*/ 
-
-  //conique cercle
-  // Eigen::VectorXd pt1(3), pt2(3), pt3(3), pt4(3), pt5(3);
-  // pt1 << 0.0, 1.0, 1.0;
-  // pt2 << 2.0, 3.0, 2.0;
-  // pt3 << -1.0, 0.0, 1.0;
-  // pt4 << 5.0, 2.0, 3.0;
-  // pt5 << -2.0, -2.0, 2.0;
-  // viewer.push_point(pt1, "p1", 200,0,0);
-  // viewer.push_point(pt2, "p2", 200,0,0);
-  // viewer.push_point(pt3, "p3", 200,0,0);
-  // viewer.push_point(pt4, "p4", 200,0,0);
-  // viewer.push_point(pt5, "p5", 200,0,0);
-
-  //conique parabole
-  // Eigen::VectorXd pt1(3), pt2(3), pt3(3), pt4(3), pt5(3);
-  // pt1 << 0.0, 0.0, 1.0;
-  // pt2 << 1.0, 2.0, -1.0;
-  // pt3 << -1.0, 1.0, 3.0;
-  // pt4 << 2.0, 0.0, 2.0;
-  // pt5 << -2.0, 3.0, 1.0;
-  // viewer.push_point(pt1, "p1", 200,0,0);
-  // viewer.push_point(pt2, "p2", 200,0,0);
-  // viewer.push_point(pt3, "p3", 200,0,0);
-  // viewer.push_point(pt4, "p4", 200,0,0);
-  // viewer.push_point(pt5, "p5", 200,0,0);
-  
-  //conique hyperbole
-  // Eigen::VectorXd pt1(3), pt2(3), pt3(3), pt4(3), pt5(3);
-  // pt1 << 1.0, 1.0, 1.0;
-  // pt2 << 2.0, -2.0, 3.0;
-  // pt3 << -1.0, 1.0, 2.0;
-  // pt4 << 3.0, 2.0, 2.0;
-  // pt5 << -2.0, -1.0, 3.0;
-  // viewer.push_point(pt1, "p1", 200,0,0);
-  // viewer.push_point(pt2, "p2", 200,0,0);
-  // viewer.push_point(pt3, "p3", 200,0,0);
-  // viewer.push_point(pt4, "p4", 200,0,0);
-  // viewer.push_point(pt5, "p5", 200,0,0);
-
-  //tangentes
-  // Eigen::VectorXd t1(3), t2(3), t3(3), t4(3), t5(3);
-  // t1<< 2.0, -3,0, 4.0;
-  // t2<< 5.0, 2,0, -8.0;
-  // t3<< 1.0, 2,0, -2.0;
-  // t4<< 3.0, -4,0, 7.0;
-  // t5<< 2.0, 2,0, -5.0;
-  // viewer.push_point(t1, "t1", 200,0,0);
-  // viewer.push_point(t2, "t2", 200,0,0);
-  // viewer.push_point(t3, "t3", 200,0,0);
-  // viewer.push_point(t4, "t4", 200,0,0);
-  // viewer.push_point(t5, "t5", 200,0,0);
-
-  //points
-  std::vector<Eigen::Vector3d> points = input_points(viewer);
-  Points P;
-  P.setter(points);
-
-  //tangentes
-  std::vector<Eigen::Vector3d> tangents = input_tangents(viewer);
-  Tangentes T;
-  T.setter(tangents);
-
-  /*// draw line anciennement le template du prof
-  viewer.push_line(pt1, pt2-pt1,  200,200,0);*/
-
-  // conic object
-  try{
-    Conic conic(P);
-    //vecteur qui contient (a, b, c, d, e, f)
-    Eigen::VectorXd coef = conic.coef();
-    viewer.push_conic(coef,"", 0,0,200);
-    //conic.display();
+  std::string ui;
+  std::cout<<"Création par points, par tangentes ou faisceaux? (p/t/f)\n";
+  std::cin>>ui;
+  while((ui!="p")&(ui!="t")&(ui!="f")){
+    std::cerr<<"Entree invalide. Recommencez\n";
+    std::cin>>ui;
   }
-  catch(const std::exception &e){
-    std::cerr<< e.what() <<"\n";
+  if(ui=="p"){
+    //Entree de Points
+    std::vector<Eigen::Vector3d> points;
+    //Soit on créé notre propre set de point soit on prend ceux donnés par le programme
+    std::cout<<"Voulez vous creer votre propre conique ? (o/n)\n";
+    std::cin>>ui;
+    while((ui!="o")&(ui!="n")){
+      std::cerr<<"Entree invalide. Recommencez\n";
+      std::cin>>ui;
+    }
+    if(ui=="o"){
+      points = input_points(viewer);
+    }
+    if(ui=="n"){
+      std::cout<<"Que voulez-vous ?\nUn cercle (c)\nUne parabole (p)\nUne hyperbole (h)\nUne ellipse (e)";
+      std::cin>>ui;
+      points = load_points(viewer,ui);
+      ui="n";
+    }
+    Points P;
+    P.setter(points);
+    //Creation de la Conique, possibles erreurs de dimensions
+    try{
+      Conic conic(P);
+      //vecteur qui contient (a, b, c, d, e, f)
+      Eigen::VectorXd coef = conic.coef();
+      std::cout<<coef<<std::endl;
+      viewer.push_conic(coef,"", 0,0,200);
+    }
+    catch(const std::exception &e){
+      std::cerr<< e.what() <<"\n";
+    }
   }
- 
+  if(ui=="t"){
+    //Entree de Tangentes
+    std::vector<Eigen::Vector3d> tangents;
+    //Soit on créer notre propre set de tangentes soit on prend celui donné par le programme
+    std::cout<<"Voulez vous creer votre propre conique ? (o/n)\n";
+    std::cin>>ui;
+    while((ui!="o")&(ui!="n")){
+      std::cerr<<"Entree invalide. Recommencez\n";
+      std::cin>>ui;
+    }
+    if(ui=="o"){
+      tangents = input_tangents(viewer);
+    }
+    else{
+      tangents = load_tangents(viewer);
+    }
+    Tangentes T;
+    T.setter(tangents);
+    //Creation de la Conique, possibles erreurs de dimensions
+    try{
+      Conic conic2(T);
+      Eigen::VectorXd coef = conic2.coef();
+      viewer.push_conic(coef,"",0,0,200);
+    }
+    catch(const std::exception &e){
+      std::cerr<<e.what() <<"\n";
+    }
+  }
+  if(ui=="f"){
+    //On cree des listes de points et on en fait des coniques.
+    Points P;
+    P.setter(load_points(viewer,"c"));
+    Conic conicA(P);
+    P.setter(load_points(viewer,"e"));
+    Conic conicB(P);
 
+    //On normalise les coniques.
+    conicA.change_coef((1/conicA.norm())*conicA.coef());
+    conicB.change_coef((1/conicB.norm())*conicB.coef());
 
-  Conic conic2(T);
-  //conic2.display();
-
-  Eigen::VectorXd coef = conic2.coef();
-  viewer.push_conic(coef,"",0,0,200);
-
-
-  //pour ajouter points dans Conic
-  //Conic Conic(points);
+    //On les affiche.
+    viewer.push_conic(conicA.coef(),"",0,0,200);
+    viewer.push_conic(conicB.coef(),"",0,200,0);
+    
+    for(int i = 0 ; i<15;i++){
+      Conic somme;
+      somme.change_matrix(cos(i*M_PI/14)*conicA.matrix()+sin(i*M_PI/14)*conicB.matrix());
+      somme.change_coef((1/somme.norm())*somme.coef());
+      viewer.push_conic(somme.coef(),"",200,0,0);
+    }
+  }
 
   // render
-  //viewer.display(); // on terminal
   viewer.render("output.html");  // generate the output file (to open with your web browser)
 
   return 0;
